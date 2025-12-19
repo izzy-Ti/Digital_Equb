a54c1249bc0ea91b891ba61eb2a29ced5ee3c9e4
israel <israelashenafi29@gmail.com>
2025-12-19 10:54:40 -0800

    member and equb structs

--- web3/contracts/Equb.sol
+++ web3/contracts/Equb.sol
@@ -7,10 +7,39 @@ contract Equb {
     struct equb{
         address owner;
         string name;
-        
+        uint256 contributionAmount;
+        uint256 cycleDuration;
+        uint256 maxMembers;
+        uint256 startTime;
+        uint256 currentRound = 0;
+        bool isActive = true;
+        address[] members;
+        address[] winners;
+        uint256 totalPool;
     }
+    struct member{
+        address wallet;
+        uint256 joinedAt;
+        bool hasPaidCurrentRound;
+        bool hasWon;
+        uint256 currentEqubs;
+    }
+    uint256 count = 0;
     mapping(uint256 => equb) public equbs;
-    function createEqub(string _name ,  ){
+    function createEqub(string _name , address _to, uint256 _contributionAmount,uint256 _cycleDuration, uint256 _maxMembers   ){
+        equb memory Equb = equbs[count];
+        Equb.owner = msg.sender;
+        Equb.name = _name;
+        Equb.
+        count ++;
+    }
+    function startEqub(){
+
+    }
+    function endEqub(){
+
+    }
+    function pauseEqub(){
 
     }
 }
