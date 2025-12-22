ad2fbdfb09e0a49b58e8837f5fceab7b761f21cf
israel <israelashenafi29@gmail.com>
2025-12-22 00:05:35 -0800

    equb funcs

--- web3/contracts/Equb.sol
+++ web3/contracts/Equb.sol
@@ -2,44 +2,92 @@
 
 pragma solidity >=0.8.2 <0.9.0;
 
-
 contract Equb {
-    struct equb{
+    struct equb {
         address owner;
         string name;
         uint256 contributionAmount;
         uint256 cycleDuration;
         uint256 maxMembers;
         uint256 startTime;
-        uint256 currentRound = 0;
-        bool isActive = true;
+        uint256 currentRound;
+        bool isActive;
         address[] members;
         address[] winners;
         uint256 totalPool;
     }
-    struct member{
+    struct member {
         address wallet;
         uint256 joinedAt;
         bool hasPaidCurrentRound;
         bool hasWon;
         uint256 currentEqubs;
     }
-    uint256 count = 0;
+    uint256 public count = 0;
     mapping(uint256 => equb) public equbs;
-    function createEqub(string _name , address _to, uint256 _contributionAmount,uint256 _cycleDuration, uint256 _maxMembers   ){
-        equb memory Equb = equbs[count];
-        Equb.owner = msg.sender;
-        Equb.name = _name;
-        Equb.
-        count ++;
+    function createEqub(
+        string memory _name,
+        uint256 _contributionAmount,
+        uint256 _cycleDuration,
+        uint256 _maxMember,
+        uint256 _startTime
+    ) public {
+        equb storage newEqub = equbs[count];
+        newEqub.owner = msg.sender;
+        newEqub.name = _name;
+        newEqub.contributionAmount = _contributionAmount;
+        newEqub.cycleDuration = _cycleDuration;
+        newEqub.maxMembers = _maxMember;
+        newEqub.startTime = _startTime;
+        newEqub.currentRound = 0;
+        newEqub.isActive = false;
+        newEqub.totalPool = 0;
+        count++;
     }
-    function startEqub(){
+    function startEqub(uint256 _equbId) public {
+        equb storage theEqub = equbs[_equbId];
+        require(
+            theEqub.owner == msg.sender,
+            "please you must be the owner to start it"
+        );
+        theEqub.isActive = true;
+        theEqub.startTime = block.timestamp;
+    }
+    function endEqub(uint256 _equbId) public {
+        equb storage theEqub = equbs[_equbId];
+        require(
+            theEqub.owner == msg.sender,
+            "please you must be the owner to end it"
+        );
+        require(
+            theEqub.members.length == theEqub.winners.length,
+            "sorry all users must win first"
+        );
+        theEqub.isActive = false;
+    }
+    function pauseEqub(uint256 _equbId) public {
+        equb storage theEqub = equbs[_equbId];
+        require(
+            theEqub.owner == msg.sender,
+            "please you must be the owner to end it"
+        );
+        theEqub.isActive = false;
+    }
+    function joinEqub(uint256 _equbId) public {
+        equb storage theEqub = equbs[_equbId];
+        theEqub.members.push(msg.sender);
+    }
+    function leaveEqub() public {}
+    function getMembers() public {}
 
-    }
-    function endEqub(){
+    function contribute() public {}
+    function getContributionStatus() public {}
+    function getTotalPool() public {}
 
-    }
-    function pauseEqub(){
+    function selectWinner() public {}
+    function getCurrentWinner() public {}
+    function getWinnersHistory() public {}
 
-    }
+    function withdrawFees() public {}
+    function emergencyStop() public {}
 }
